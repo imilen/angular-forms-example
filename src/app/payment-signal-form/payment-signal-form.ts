310fa35986371e7131d04a70993864e29be23175
imilen <m.ivanov@email.com>
2026-05-22 09:37:33 +0300

    feat(payment-signal-form): add component

---
+++ src/app/payment-signal-form/payment-signal-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-signal-form',
+  imports: [],
+  templateUrl: './payment-signal-form.html',
+  styleUrl: './payment-signal-form.css',
+})
+export class PaymentSignalForm {}
