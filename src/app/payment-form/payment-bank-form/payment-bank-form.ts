afa0c04283a8e37309dd6fec80cfb09ebbb3c12a
imilen <m.ivanov@email.com>
2026-05-24 18:24:49 +0300

    feat(payment-form): add payment-bank-form and payment-card-form components

---
+++ src/app/payment-form/payment-bank-form/payment-bank-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-bank-form',
+  imports: [],
+  templateUrl: './payment-bank-form.html',
+  styleUrl: './payment-bank-form.css',
+})
+export class PaymentBankForm {}
---
+++ src/app/payment-form/payment-card-form/payment-card-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-card-form',
+  imports: [],
+  templateUrl: './payment-card-form.html',
+  styleUrl: './payment-card-form.css',
+})
+export class PaymentCardForm {}
