5911473f31444317d4904ae2e6ac1e0bca10237e
imilen <m.ivanov@email.com>
2026-05-25 14:18:48 +0300

    feat(payment-card-form): add validation rules display for all fields

--- src/app/payment-form/payment-card-form/payment-card-form.ts
+++ src/app/payment-form/payment-card-form/payment-card-form.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import type { FieldTree } from '@angular/forms/signals';
 import { FormField } from '@angular/forms/signals';
-import { isFieldError, isFieldSuccess } from '../payment-form-field.utils';
+import { findError, isFieldError, isFieldSuccess } from '../payment-form-field.utils';
 import { CardModel } from '../payment-form.models';
+import { CART } from './payment-card-form.constants';
 
 @Component({
   selector: 'app-payment-card-form',
@@ -13,9 +14,11 @@ import { CardModel } from '../payment-form.models';
 })
 export class PaymentCardForm {
   cardForm = input.required<FieldTree<CardModel>>();
+  protected readonly cart = CART;
 
   protected readonly validation = {
     success: isFieldSuccess,
     error: isFieldError,
+    findError: findError,
   };
 }
