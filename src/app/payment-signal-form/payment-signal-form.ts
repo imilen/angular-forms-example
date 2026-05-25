6fc75e1a3d6259068ff1ae65af3d2a9209a68c17
imilen <m.ivanov@email.com>
2026-05-25 09:09:55 +0300

    refactor(payment-signal-form): remove component

--- src/app/payment-signal-form/payment-signal-form.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-payment-signal-form',
-  imports: [],
-  templateUrl: './payment-signal-form.html',
-  styleUrl: './payment-signal-form.css',
-})
-export class PaymentSignalForm {}
